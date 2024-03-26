4e9307693a5d5ae34f8686d2425e338602cd5296
Alex <ajlevy246@vt.edu>
2024-03-26 17:36:19 -0400

    Further testing

--- montecarlo/__init__.py
+++ montecarlo/__init__.py
@@ -1,5 +1,1 @@
 """Implements MonteCarlo for use in Ising-Hamiltonian Model"""
-
-# Add imports here
-from BitString import *
-
--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,10 +1,11 @@
 """Provide the primary functions."""
 
-
 from BitString import *
 
 def test():
-    print("Working!")
+    testBitString = BitString(5)
+    testBitString.set_int_config(1024)
+    print("Should be 1024: ", testBitString.int())
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
