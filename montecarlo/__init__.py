eb1e4b93bcff0af103558fcc40661a1806337787
ajlevy246 <ajlevy246@gmail.com>
2024-04-18 11:09:07 -0400

    Committing docs changes

--- montecarlo/__init__.py
+++ montecarlo/__init__.py
@@ -1,6 +1,7 @@
 """Implements MonteCarlo for use in Ising-Hamiltonian Model"""
 
-from montecarlo.isinghamiltonian import IsingHamiltonian
+from .isinghamiltonian import IsingHamiltonian
+
 
 if __name__ == "__main__":
     print("Running as main")
